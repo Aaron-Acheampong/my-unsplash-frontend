884916e0a028748e62864aa56596d6da1ff8d480
Aaron-Acheampong <aronzy.as@gmail.com>
2023-03-18 18:01:11 +0000

    Config Latest

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const baseUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3300"
-    : "https://my-unsplash-api.onrender.com";
+    : "https://my-unsplash-api.onrender.com/";
